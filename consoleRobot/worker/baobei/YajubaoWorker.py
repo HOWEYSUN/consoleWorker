170998b7ad7d76143406cf8ca2bed40a8b9aacde
china9966 <china9966@163.com>
2020-12-02 08:40:01 +0800

    add yajubaoWorker

---
+++ consoleRobot/worker/baobei/YajubaoWorker.py
@@ -0,0 +1,9 @@
+from worker.basicWorker import BasicWorker
+
+
+class YajubaoWorker(BasicWorker):
+    def __init__(self):
+        # 01集团
+        # 04客服中心
+        # 001自增编号
+        super().__init__('WF0104001')
